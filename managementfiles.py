0136b22c51ee8b50e6be9ea8c1afe2ebb518d7e9
Irisha <irisha.kostevich@mail.ru>
2023-08-15 17:04:29 +0300

    New: updatinf files.

--- managementfiles.py
+++ managementfiles.py
@@ -1,14 +1,17 @@
 import os
-def management(article, articles):
+def delete_files(article, articles):
 	count = 0
-	for el in articles:	
-		if article.image in el.image:
+	for articleview in articles:	
+		if article.image in articleview.image:
 			count +=1
-			print(count)
 	if count < 2:
-		print(count)
 		os.remove((f"static//uploadimages//{article.image}"))
 		
-    
-    
 
+def updating_files(article, articles, articleold):
+	countold = 0
+	for articleview in articles:
+		if articleold in articleview.image:
+			countold +=1
+	if countold < 1:
+		os.remove((f"static//uploadimages//{articleold}"))
